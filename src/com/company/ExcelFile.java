880df3c2a7fa2cec6cc711de456955557d813334
tv.unimol <doslovno@gmail.com>
2017-02-02 10:39:57 +0300

    add formatted function in ExcelFile and add idea files

--- src/com/company/ExcelFile.java
+++ src/com/company/ExcelFile.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -26,6 +27,10 @@ public class ExcelFile {
         sheet = workbook.getSheetAt(0);
     }
 
+    /**
+     * Remove(shift) row.
+     * @param rowNo row number to be deleted
+     */
     private void deleteRow(int rowNo) {
         if (sheet == null) {
             return;
@@ -42,29 +47,142 @@ public class ExcelFile {
         }
     }
 
-    // TODO: убирать строки с нулями в количестве и менять кол-во больше чем 1 на 1
+    /**
+     * Remove rows with duplicate models.
+     * @throws IOException
+     */
     public void removeDuplicates() throws IOException {
-        String tempValue = "";
+        formatTable();
         for (int i = 1; i <= sheet.getLastRowNum(); ++i) {
-            String cellValue = sheet.getRow(i).getCell(0).toString();
-            if (cellValue.equals(tempValue)) {
+            // Если артикул и цвет равны - удалить строку
+            if (getCellValue(i, 0).equals(getCellValue(i - 1, 0))
+                    && getCellValue(i, 1).equals(getCellValue(i - 1, 1))) {
                 deleteRow(i);
                 --i;
             } else {
-                tempValue = cellValue;
-                Cell cell8 = sheet.getRow(i).getCell(8);
-                cell8.setCellValue(QueryUtils.fetchMaxPrice(cellValue));
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                Cell cell3 = sheet.getRow(i).getCell(3);
+                //поставить "кол-во" - 1
+                cell3.setCellValue(1);
             }
         }
         closeTable();
         System.out.println("Подстановка цен завершена.");
     }
 
+    /**
+     * Возвращает строку, находящуюся на пересечении номера строки и столбца
+     */
+    private String getCellValue(int rowNumber, int cellNumber) {
+        return sheet.getRow(rowNumber).getCell(cellNumber).toString();
+    }
+
+    /**
+     * Проставляет цену для артикула(первая ячейка в строке) в пятой колонке таблицы.
+     *
+     */
+    public void putPrices() throws IOException{
+        for (Row row : sheet) {
+            String modelCell = row.getCell(0).toString();
+            Cell priceCell = row.getCell(4);
+            priceCell.setCellType(CellType.NUMERIC);
+            priceCell.setCellValue(QueryUtils.fetchMaxPrice(modelCell));
+            try {
+                // не знаю, как сервер отреагирует, на всякий случай
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        closeTable();
+    }
+
+    /** Удаляет столбцы "Название", "Цена", "Ск%", "Цена со ск.", "Ст.цена",
+     *  меняет название стобца "Кол-во" на "Количество"
+     */
+     private void formatTable() {
+         deleteColumn(sheet, 3);
+         deleteColumn(sheet, 4);
+         deleteColumn(sheet, 5);
+         deleteColumn(sheet, 6);
+         deleteColumn(sheet, 7);
+
+         Cell count = sheet.getRow(0).getCell(3);
+         count.setCellValue("Количество");
+
+    }
+
+    /**
+     * Given a sheet, this method deletes a column from a sheet and moves
+     * all the columns to the right of it to the left one cell.
+     *
+     * Note, this method will not update any formula references.
+     */
+    public static void deleteColumn(Sheet sheet, int columnToDelete){
+        int maxColumn = 0;
+        for ( int r=0; r < sheet.getLastRowNum()+1; r++ ){
+            Row row = sheet.getRow( r );
+
+            // if no row exists here; then nothing to do; next!
+            if ( row == null )
+                continue;
+
+            // if the row doesn't have this many columns then we are good; next!
+            int lastColumn = row.getLastCellNum();
+            if ( lastColumn > maxColumn )
+                maxColumn = lastColumn;
+
+            if ( lastColumn < columnToDelete )
+                continue;
+
+            for ( int x=columnToDelete+1; x < lastColumn + 1; x++ ){
+                Cell oldCell    = row.getCell(x-1);
+                if ( oldCell != null )
+                    row.removeCell( oldCell );
+
+                Cell nextCell   = row.getCell( x );
+                if ( nextCell != null ){
+                    Cell newCell    = row.createCell( x-1, nextCell.getCellType() );
+                    cloneCell(newCell, nextCell);
+                }
+            }
+        }
+        // Adjust the column widths
+        for ( int c=0; c < maxColumn; c++ ){
+            sheet.setColumnWidth( c, sheet.getColumnWidth(c+1) );
+        }
+    }
+
+    /**
+     * Takes an existing Cell and merges all the styles and forumla
+     * into the new one
+     */
+    private static void cloneCell(Cell cNew, Cell cOld){
+        cNew.setCellComment(cOld.getCellComment());
+        cNew.setCellStyle(cOld.getCellStyle());
+
+        switch ( cNew.getCellTypeEnum() ){
+            case BOOLEAN:{
+                cNew.setCellValue( cOld.getBooleanCellValue() );
+                break;
+            }
+            case NUMERIC:{
+                cNew.setCellValue( cOld.getNumericCellValue() );
+                break;
+            }
+            case STRING:{
+                cNew.setCellValue( cOld.getStringCellValue() );
+                break;
+            }
+            case ERROR:{
+                cNew.setCellValue( cOld.getErrorCellValue() );
+                break;
+            }
+            case FORMULA:{
+                cNew.setCellFormula( cOld.getCellFormula() );
+                break;
+            }
+        }
+    }
 
     private void closeTable() throws IOException {
         // Write the output to a file
