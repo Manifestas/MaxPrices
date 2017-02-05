438f1a28aa580a1a87524326982f550355139fd8
tv.unimol <doslovno@gmail.com>
2017-02-05 12:01:53 +0300

    init in Main class now, remove MaxPrices.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        MaxPrices maxPrices = new MaxPrices();
+        try {
+            // set look and feel to system dependent
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Gui().initUi();
+            }
+        });
     }
 }
